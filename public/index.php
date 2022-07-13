6c64f3e689fb1f4ba7a75cda92216731fdb66519
Kharlamov Vladislav <kharlamov.workspace@gmail.com>
2022-07-13 13:12:59 +0300

    Initial Commit

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<?php
+define("BASEDIR", dirname(__DIR__));
+
+require_once BASEDIR . "/vendor/autoload.php";
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+
+$app->get("/hello/{name}", function(Request $request, Response $response, array $args) {
+	$name = $args["name"];
+	$response->getBody()->write("Hello, $name!");
+	return $response;
+});
+
+$app->run();
